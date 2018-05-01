d4e449c1b982c758bd0bd5dd03b8ac5bfe00009a
Malachi McIntosh <mmcintosh@planittesting.com>
2018-05-01 16:08:32 +1200

    Set up

---
+++ VaultAutomation/PageObjects/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaultAutomation.PageObjects
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [FindsBy(How = How.CssSelector, Using = "#user")]
+        private IWebElement userName;
+
+        [FindsBy(How = How.CssSelector, Using = "#password")]
+        private IWebElement password;
+
+        [FindsBy(How = How.CssSelector, Using = "#login-submit")]
+        private IWebElement loginButton;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void NavigateToLoginPage()
+        {
+            driver.Navigate().GoToUrl("https://alphav3.vaultintel.com/index/hostLogin");
+        }
+
+        public HomePage Login(String Username, String Password)
+        {
+            userName.SendKeys(Username);
+            password.SendKeys(Password);
+            loginButton.Click();
+            return new HomePage(driver);
+        }
+
+    }
+}
---
+++ VaultAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VaultAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("VaultAutomation")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1c704ee7-8af3-4e87-bf0a-5e3a286b4c7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
