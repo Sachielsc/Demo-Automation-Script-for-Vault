6c32d71c0aabeda56259259937d5909d8d2a04db
Malachi McIntosh <mmcintosh@planittesting.com>
2018-05-03 13:00:44 +1200

    Created ReportLog wrapper class. Work on the search by ref id method.

--- TestVault/TestVault.Test/NUnitVaultTest.cs
+++ TestVault/TestVault.Test/NUnitVaultTest.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using System.Web;
-using System.Net;
-using AventStack.ExtentReports;
-using AventStack.ExtentReports.Reporter;
-using AventStack.ExtentReports.Reporter.Configuration;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
 using TestVault.PageObjects;
 using TestVault.Reports;
@@ -19,103 +13,118 @@ using TestVault.PageObjects;
 
 namespace TestVault.Test
 {
-	[TestFixture]
+    /// <summary>
+    /// This is a test fixture containing two test cases related to the Events section of th the Vault Intelligence website.
+    /// </summary>
+    /// <author>
+    /// Malachi McIntosh and Charles Shu 2018
+    /// </author>
+    [TestFixture]
 	public class NUnitVaultTest
 	{
 		IWebDriver driver;
-		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-	    public static ExtentReports extent;
-	    public static ExtentTest test;
 
+        /// <summary>
+        /// Initialise the ChromeDriver.
+        /// </summary>
         [SetUp]
 		public void Init()
 		{
-            //extent = ExtentManager.GetExtent();
             ChromeOptions options = new ChromeOptions();
 			options.AddArguments("--start-maximized");
 			options.AddArguments("disable-infobars");
 			driver = new ChromeDriver(options);
-
-
-
-
-            //#############################################################////////////////////////////////
-		    var dir = "..\\TestVault\\TestVault\\Reports/";
-		    var fileName = "Extent.html";
-		    var htmlReporter = new ExtentHtmlReporter(dir + fileName);
-		    htmlReporter.Configuration().Theme = Theme.Dark;
-		    htmlReporter.Configuration().ChartLocation = ChartLocation.Top;
-		    htmlReporter.Configuration().ChartVisibilityOnOpen = true;
-		    htmlReporter.Configuration().DocumentTitle = "Extent/Vault Automation";
-		    htmlReporter.Configuration().ReportName = "Extent/Add an Event";
-
-            extent = new ExtentReports();
-		    extent.AddSystemInfo("Tester", "Malachi McIntosh");
-		    extent.AddSystemInfo("Tester", "Charles Shu");
-            extent.AddSystemInfo("OS", "Windows 10");
-		    extent.AddSystemInfo("Browser", "Google Chrome");
-		    extent.AddSystemInfo("Date/Time", DateTime.Now.ToString());
-            extent.AttachReporter(htmlReporter);
-		    //#############################################################\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-
         }
 
+        /// <summary>
+        /// This is an end-to-end test case for adding an event from the portal.
+        /// </summary>
         [Test]
-		public void AddAnEventItemViaPortal()
+        public void AddAnEventItemViaPortal()
 		{
-            //Set up the test in ExtentReporter
-		    test = extent.CreateTest("AddAnEventItemViaPortal", "This is an end-to-end test case regarding the adding of an Event via the Portal.");
-		    test.Info("Navigated to portal page.");
-		    var portalPage = new PageObjects.PortalPage(driver);                    //Add from portal.
-		    portalPage.NavigateToPortalPage();
-            portalPage.ReportAnInjury();                                            //Fill out and save a new report.
-		    test.Info("Filled out report an injury");
-            var loginPage = new LoginPage(driver);
-            loginPage.NavigateToLoginPage();                                        //Login in order to check Event has been added.
-            test.Info("Navigated to login page.");
-		    var homePage = loginPage.LoginWithCredentials("plan.8", "plan01#");
-		    test.Info("Submitted login details");
-            var eventsPage = new EventsPage(driver);                                //Now check if the event has been added.
-		    eventsPage.NavigateToEventsPage();
-		    test.Info("Navigated to Events page");
-            eventsPage.SearchByReferenceID(portalPage.GetReferenceID());
-		    test.Info("Searched for reference");
-		    eventsPage.confirmEventAdded(portalPage.GetReferenceID());              //Confirm Event has been added.
-		    test.Pass("Event successfully added");
+		    try
+		    {
+		        // Set up the test in ReportLog wrapper class.
+		        ReportLog.CreateTest("AddAnEventItemViaPortal",
+		            "This is an end-to-end test case regarding the adding of an Event via the Portal.");
+		        // Add from portal.
+		        var portalPage = new PortalPage(driver);
+		        portalPage.NavigateToPortalPage();
+		        ReportLog.Log("Navigated to portal page.");
+		        // Fill out and save a new report.
+		        portalPage.ReportAnInjury();
+		        ReportLog.Log("Filled out report an injury.");
+		        // Login in order to check Event has been added.
+		        var loginPage = new LoginPage(driver);
+		        loginPage.NavigateToLoginPage();
+		        ReportLog.Log("Navigated to login page.");
+		        var homePage = loginPage.LoginWithCredentials("plan.8", "plan01#");
+		        ReportLog.Log("Submitted login details.");
+		        // Now check if the event has been added.
+		        var eventsPage = new EventsPage(driver);
+		        eventsPage.NavigateToEventsPage();
+		        ReportLog.Log("Navigated to Events page.");
+		        eventsPage.SearchByReferenceID(portalPage.GetReferenceID());
+		        ReportLog.Log("Searched for reference");
+		        try
+		        {
+		            // Confirm Event has been added.
+ 		            eventsPage.confirmEventAdded(portalPage.GetReferenceID());
+		            ReportLog.Pass("AddAnEventItemViaPortal Test Passed.");
+		        }
+		        catch (AssertionException a)
+		        {
+		            ITakesScreenshot screenshot = (ITakesScreenshot) driver;
+                    // Test failed due to assertion error.
+		            ReportLog.Fail(a.Message);
+		            throw a;
+		        }
+		    }
+		    catch (Exception e)
+		    {
+		        // Test failed due to unforeseen exception.
+                ReportLog.Fail(e.Message + "\n" + e.StackTrace);
+		        throw e;
+		    }
 		}
 
 		[Test]
-		public void EditAnEventItem() /* Page oriented model */
+		public void EditAnEventItem()
 		{
-			// log in
-			LoginPage loginPage = new LoginPage(driver);
+		    // Set up the test in ReportLog wrapper class.
+		    ReportLog.CreateTest("EditAnEventItem", "This is an end-to-end test case regarding the editing of an Event.");
+            // Log in.
+            LoginPage loginPage = new LoginPage(driver);
 			loginPage.NavigateToLoginPage();
+            ReportLog.Log("Navigated to Login Page.");
 			loginPage.TypeUserName("plan.6");
 			loginPage.TypePassword("plan01#");
+		    ReportLog.Log("Entered credentials.");
 
-			// go to home page
-			HomePage homePage = loginPage.ConfirmLoginAndGoBackToHomePage();
+            // Go to home page.
+            HomePage homePage = loginPage.ConfirmLoginAndGoBackToHomePage();
 			homePage.WaitUntilHomePageLoadingComplete();
+		    ReportLog.Log("Login confirmed and redirected back to Home Page.");
 
-			// go to events page
-			EventsPage eventsPage = homePage.GoToEventsPage();
+            // Go to events page
+            EventsPage eventsPage = homePage.GoToEventsPage();
 			eventsPage.WaitUntilEventsPageLoadingComplete();
+		    ReportLog.Log("Navigated to Events Page.");
 
-			// go to event item page
-			EventsItemPage eventsItemPage = eventsPage.GoToSpecificEventsItemPage(0);
+            // go to event item page
+            EventsItemPage eventsItemPage = eventsPage.GoToSpecificEventsItemPage(0);
 			eventsItemPage.WaitUntilEventsItemPageLoadingComplete();
 
 			// input mandatory values
 			eventsItemPage.InputMandatoryValues();
-		}
+		    ReportLog.Log("Inputted mandatory fields.");
+		    ReportLog.Pass("EditAnEventItem Test Passed.");
+        }
 
 		[TearDown]
 		public void CleanUp()
 		{
-            extent.Flush();
-			log.Info("Test Completed!");
+            ReportLog.Flush();
 			driver.Quit();
 		}
 	}
