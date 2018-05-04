8dc2801433d235f1c0e3baea31f01998c4b55c32
Charles Shu <cshu@planittesting.com>
2018-05-04 16:26:38 +1200

    1. delete specFlow files 2. minor changes in gitignore 3. complete the selection assertion method 4. create the folder "ErrorScreenshots"

--- TestVault/TestVault/PageObjects/EventsItemPage.cs
+++ TestVault/TestVault/PageObjects/EventsItemPage.cs
@@ -76,8 +76,8 @@ namespace TestVault.PageObjects
 
 			SelectElement category = new SelectElement(driver.FindElement(By.Id("category")));
 			Task.Delay(400).Wait();
-			category.SelectByIndex(30);
-			ReportLog.SelectAssert("Strain", category, driver, "category selection");
+			category.SelectByText("Strain123123");
+			ReportLog.SelectAssert("Strain123123", category, driver, "category selection");
 
 			SelectElement severity = new SelectElement(driver.FindElement(By.Id("severityid")));
 			severity.SelectByText("Between Life and Death");
